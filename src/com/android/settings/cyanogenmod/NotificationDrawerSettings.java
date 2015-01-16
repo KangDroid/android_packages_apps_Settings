90431dc2140e922371c49ab3cb8223214e3016ee
KangDroid <kangdroid@naver.com>
2015-01-16 19:09:49 +0000

    Revert "Fix for KDP"
    This reverts commit 243676d6b12993d917ceb8b27953bda99764c78e.

--- src/com/android/settings/cyanogenmod/NotificationDrawerSettings.java
+++ src/com/android/settings/cyanogenmod/NotificationDrawerSettings.java
@@ -21,18 +21,8 @@ import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.SwitchPreference;
 import android.provider.Settings;
-import android.content.pm.PackageManager;
-import android.content.res.Resources;
-import android.media.Ringtone;
-import android.media.RingtoneManager;
-import android.net.Uri;
-import android.preference.ListPreference;
-import android.preference.PreferenceScreen;
-import android.text.TextUtils;
-import android.view.View;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
-import com.android.internal.util.crdroid.DeviceUtils;
 
 import java.util.Locale;
 
@@ -42,20 +32,14 @@ public class NotificationDrawerSettings extends SettingsPreferenceFragment
     private static final String TAG = "NotificationDrawer";
 
     private static final String TOGGLE_MAIN_TILES = "qs_main_tiles";
-    private static final String PRE_QUICK_PULLDOWN = "quick_pulldown";
-	private static final String PREF_SMART_PULLDOWN = "smart_pulldown";
 
     SwitchPreference mToggleMainTiles;
-    ListPreference mQuickPulldown;
-	private ListPreference mSmartPulldown;
 
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         addPreferencesFromResource(R.xml.notification_drawer_settings);
-		
-		PreferenceScreen prefs = getPreferenceScreen();
-		
+
         mToggleMainTiles = (SwitchPreference) findPreference(TOGGLE_MAIN_TILES);
         mToggleMainTiles.setOnPreferenceChangeListener(this);
 
@@ -64,36 +48,6 @@ public class NotificationDrawerSettings extends SettingsPreferenceFragment
                 1, UserHandle.myUserId()) == 1;
 
         mToggleMainTiles.setChecked(useMainTiles);
-		
-		// KDP's Addition
-		mSmartPulldown = (ListPreference) findPreference(PREF_SMART_PULLDOWN);
-		if (!DeviceUtils.isPhone(getActivity())) {
-			prefs.removePreference(mSmartPulldown);
-		} else {
-            // Smart Pulldown
-            mSmartPulldown.setOnPreferenceChangeListener(this);
-            int smartPulldown = Settings.System.getInt(getContentResolver(),
-                    Settings.System.QS_SMART_PULLDOWN, 0);
-            mSmartPulldown.setValue(String.valueOf(smartPulldown));
-            updateSmartPulldownSummary(smartPulldown);
-		}
-
-        mQuickPulldown = (ListPreference) findPreference(PRE_QUICK_PULLDOWN);
-        if (!DeviceUtils.isPhone(getActivity())) {
-            prefs.removePreference(mQuickPulldown);
-        } else {
-            // Quick Pulldown
-            mQuickPulldown.setOnPreferenceChangeListener(this);
-            int statusQuickPulldown = Settings.System.getInt(getContentResolver(),
-                    Settings.System.STATUS_BAR_QUICK_QS_PULLDOWN, 1);
-            mQuickPulldown.setValue(String.valueOf(statusQuickPulldown));
-            updateQuickPulldownSummary(statusQuickPulldown);
-        }
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
     }
 
     @Override
@@ -103,63 +57,8 @@ public class NotificationDrawerSettings extends SettingsPreferenceFragment
                     getActivity().getContentResolver(), Settings.Secure.QS_USE_MAIN_TILES,
                     ((Boolean) newValue) ? 1 : 0, UserHandle.myUserId());
             return true;
-         } else if (preference == mSmartPulldown) {
-             int smartPulldown = Integer.valueOf((String) newValue);
-             Settings.System.putInt(getContentResolver(),
-                     Settings.System.QS_SMART_PULLDOWN,
-                     smartPulldown);
-             updateSmartPulldownSummary(smartPulldown);
-             return true;
- 		} else if (preference == mQuickPulldown) {
-             int statusQuickPulldown = Integer.valueOf((String) newValue);
-             Settings.System.putInt(getContentResolver(),
-                     Settings.System.STATUS_BAR_QUICK_QS_PULLDOWN,
-                     statusQuickPulldown);
-             updateQuickPulldownSummary(statusQuickPulldown);
-             return true;
- 		}
+        }
         return false;
-    }
-
-    private void updateSmartPulldownSummary(int value) {
-        Resources res = getResources();
-
-        if (value == 0) {
-            // Smart pulldown deactivated
-            mSmartPulldown.setSummary(res.getString(R.string.smart_pulldown_off));
-        } else {
-            String type = null;
-            switch (value) {
-                case 1:
-                    type = res.getString(R.string.smart_pulldown_dismissable);
-                    break;
-                case 2:
-                    type = res.getString(R.string.smart_pulldown_persistent);
-                    break;
-                default:
-                    type = res.getString(R.string.smart_pulldown_all);
-                    break;
-            }
-            // Remove title capitalized formatting
-            type = type.toLowerCase();
-            mSmartPulldown.setSummary(res.getString(R.string.smart_pulldown_summary, type));
-        }
-	}
-
-    private void updateQuickPulldownSummary(int value) {
-        Resources res = getResources();
-
-        if (value == 0) {
-            // quick pulldown deactivated
-            mQuickPulldown.setSummary(res.getString(R.string.quick_pulldown_off));
-        } else {
-            Locale l = Locale.getDefault();
-            boolean isRtl = TextUtils.getLayoutDirectionFromLocale(l) == View.LAYOUT_DIRECTION_RTL;
-            String direction = res.getString(value == 2
-                    ? (isRtl ? R.string.quick_pulldown_right : R.string.quick_pulldown_left)
-                    : (isRtl ? R.string.quick_pulldown_left : R.string.quick_pulldown_right));
-            mQuickPulldown.setSummary(res.getString(R.string.summary_quick_pulldown, direction));
-        }
     }
 
 }
