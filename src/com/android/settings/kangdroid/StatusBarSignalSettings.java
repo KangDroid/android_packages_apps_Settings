1390372deaeeacad277ee7123c6756b4e8a23a40
KangDroid <kangdroid@naver.com>
2015-01-17 16:55:17 +0000

    Caught a error

--- src/com/android/settings/kangdroid/StatusBarSignalSettings.java
+++ src/com/android/settings/kangdroid/StatusBarSignalSettings.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.settings.bliss;
+package com.android.settings.kangdroid;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
