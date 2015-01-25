92a8b970c2db2de8a7b2203c0d4fb85137c6226c
KangDroid <kangdroid@naver.com>
2015-01-25 01:45:21 +0000

    Maybe fix
    Try this

--- src/com/android/settings/rr/LockSettings.java
+++ src/com/android/settings/rr/LockSettings.java
@@ -45,12 +45,10 @@ import com.android.settings.Utils;
 public class LockSettings extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
 	
 	private static final String KEY_LOCKSCREEN_CAMERA_WIDGET_HIDE = "camera_widget_hide";
-	private static final String KEY_LOCKSCREEN_DIALER_WIDGET_HIDE = "dialer_widget_hide";
 	private static final String KEY_LOCKSCREEN_WEATHER = "lockscreen_weather";
 	
 	private SwitchPreference mCameraWidgetHide;
 	private PreferenceScreen mLockScreen;
-	private SwitchPreference mDialerWidgetHide;
 	private SwitchPreference mLockscreenWeather;
 	
 	private Context mContext;
@@ -80,12 +78,6 @@ public class LockSettings extends SettingsPreferenceFragment implements OnPrefer
         if (mCameraDisabled){
             mLockScreen.removePreference(mCameraWidgetHide);
         }
-
-        // Dialer widget hide
-        mDialerWidgetHide = (SwitchPreference) findPreference("dialer_widget_hide");
-        if ((!Utils.isVoiceCapable(mContext) || Utils.isWifiOnly(mContext))) {
-            mLockScreen.removePreference(mDialerWidgetHide);
-        }
 		
         // Lockscreen weather
         mLockscreenWeather = (SwitchPreference) findPreference(KEY_LOCKSCREEN_WEATHER);
@@ -110,12 +102,6 @@ public class LockSettings extends SettingsPreferenceFragment implements OnPrefer
 
 
     public boolean onPreferenceChange(Preference preference, Object objValue) {
-		if (preference == mLockscreenWeather) {
-        boolean value = (Boolean) objValue;
-        Settings.System.putIntForUser(getActivity().getContentResolver(),
-                Settings.System.LOCKSCREEN_WEATHER, value ? 1 : 0, UserHandle.USER_CURRENT);
-        Helpers.restartSystemUI();
-	}
         return false;
     }
 
