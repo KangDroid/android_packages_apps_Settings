59d852a98f1b46dd6764a545002011bb2b0839c8
KangDroid <hyunwoo286@gmail.com>
2015-02-20 18:15:57 +0000

    Re-Re Organization
    This commit is trying to re-back deleted function and organize again to Make user eye more comfortable.
    
    PS1: Make KDP Custom Setting and Theme Settings to advanced-mode
    PS2: Add-Back Gesture Lock Settings
    PS3: Add-Back pin keypad shuffler
    PS4: TEST: Disable DashBoard Category
             : this one is killing our eyes too.
    PS5: KangDroid Info Settings are also should be Advanced Settings.
    PS6: Trying to Fix build error and Add some Advanced settings lines.
    PS7: PS6: Add Activity in AndroidManifest.xml Still causes a error.
    PS8: Fix Compilation Error.
    PS9: PS9=PS8
    
    TODO: 1. Make more like AOSP Style(So it doesn't hurt Public's eyes lol)
          2. put more advanced settings on my customization settings.
    
    Change-Id: Ib48d9fcb9dfc98e3fa0dd49dd6b08629d70f2f2e

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -109,5 +109,6 @@ public class Settings extends SettingsActivity {
     public static class ExpandedDesktopSettingsActivity extends SettingsActivity { /* empty */ }
     public static class SoundSettingsActivity extends SettingsActivity { /* empty */ }
     public static class LockScreenSettingsActivity extends SettingsActivity { /* empty */ }
+	public static class VolumeStepsActivity extends SettingsActivity { /* empty */ }
 }
 
