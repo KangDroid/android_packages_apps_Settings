8eae6f7bc28c15b5bc16762153c4643c69deebb6
KangDroid <hyunwoo286@gmail.com>
2015-02-28 13:46:31 +0000

    Remove Smart Pull Down and Re-Implement Quick Pulldown
    Smart Pull Down is not usuable anymore, Quick PullDown is take care of them.
    
    Change-Id: Ia8976a4eecb1492fc65611b6fc41587df325b9c6

--- src/com/android/settings/cyanogenmod/NotificationDrawerSettings.java
+++ src/com/android/settings/cyanogenmod/NotificationDrawerSettings.java
@@ -16,6 +16,8 @@
 package com.android.settings.cyanogenmod;
 
 import android.os.Bundle;
+import android.os.UserHandle;
+import android.content.ContentResolver;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.content.pm.PackageManager;
@@ -27,6 +29,7 @@ import android.preference.ListPreference;
 import android.preference.PreferenceScreen;
 import android.text.TextUtils;
 import android.view.View;
+import android.provider.Settings;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
@@ -35,47 +38,35 @@ import com.android.internal.util.crdroid.DeviceUtils;
 import android.provider.Settings;
 import java.util.Locale;
 
-public class NotificationDrawerSettings extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
-    private static final String PRE_QUICK_PULLDOWN = "quick_pulldown";
-	private static final String PREF_SMART_PULLDOWN = "smart_pulldown";
+public class NotificationDrawerSettings extends SettingsPreferenceFragment implements Preference.OnPreferenceChangeListener {
+	private static final String QUICK_PULLDOWN = "quick_pulldown";
 	
     private Preference mQSTiles;
-    ListPreference mQuickPulldown;
-	private ListPreference mSmartPulldown;
+	private ListPreference mQuickPulldown;
 
     @Override
-    public void onCreate(Bundle icicle) {
-        super.onCreate(icicle);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.notification_drawer_settings);
 		
 		PreferenceScreen prefs = getPreferenceScreen();
 
         mQSTiles = findPreference("qs_order");
-		
-		// KDP's Addition
-		mSmartPulldown = (ListPreference) findPreference(PREF_SMART_PULLDOWN);
-		if (!DeviceUtils.isPhone(getActivity())) {
-			prefs.removePreference(mSmartPulldown);
-		} else {
-            // Smart Pulldown
-            mSmartPulldown.setOnPreferenceChangeListener(this);
-            int smartPulldown = Settings.System.getInt(getContentResolver(),
-                    Settings.System.QS_SMART_PULLDOWN, 0);
-            mSmartPulldown.setValue(String.valueOf(smartPulldown));
-            updateSmartPulldownSummary(smartPulldown);
-		}
+    }
 
-         mQuickPulldown = (ListPreference) findPreference(PRE_QUICK_PULLDOWN);
-        if (!DeviceUtils.isPhone(getActivity())) {
-            prefs.removePreference(mQuickPulldown);
-        } else {
-            // Quick Pulldown
-            mQuickPulldown.setOnPreferenceChangeListener(this);
-            int statusQuickPulldown = Settings.System.getInt(getContentResolver(),
-                    Settings.System.STATUS_BAR_QUICK_QS_PULLDOWN, 1);
-            mQuickPulldown.setValue(String.valueOf(statusQuickPulldown));
-            updateQuickPulldownSummary(statusQuickPulldown);
-        }
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        PreferenceScreen prefSet = getPreferenceScreen();
+        ContentResolver resolver = getActivity().getContentResolver();
+        mQuickPulldown = (ListPreference) prefSet.findPreference(QUICK_PULLDOWN);
+
+        mQuickPulldown.setOnPreferenceChangeListener(this);
+        int quickPulldownValue = Settings.System.getIntForUser(resolver,
+                Settings.System.QS_QUICK_PULLDOWN, 0, UserHandle.USER_CURRENT);
+        mQuickPulldown.setValue(String.valueOf(quickPulldownValue));
+        updatePulldownSummary(quickPulldownValue);
     }
 
     @Override
@@ -87,64 +78,31 @@ public class NotificationDrawerSettings extends SettingsPreferenceFragment imple
                     qsTileCount, qsTileCount));
     }
 	
+    @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-		if (preference == mSmartPulldown) {
-		             int smartPulldown = Integer.valueOf((String) newValue);
-		             Settings.System.putInt(getContentResolver(),
-		                     Settings.System.QS_SMART_PULLDOWN,
-		                     smartPulldown);
-		             updateSmartPulldownSummary(smartPulldown);
-		             return true;
-		 		} else if (preference == mQuickPulldown) {
-		             int statusQuickPulldown = Integer.valueOf((String) newValue);
-		             Settings.System.putInt(getContentResolver(),
-		                     Settings.System.STATUS_BAR_QUICK_QS_PULLDOWN,
-		                     statusQuickPulldown);
-		             updateQuickPulldownSummary(statusQuickPulldown);
-		             return true;
-		 		}
-		        return false;
-		    }
-			
-		    private void updateSmartPulldownSummary(int value) {
-		        Resources res = getResources();
+        ContentResolver resolver = getContentResolver();
+        if (preference == mQuickPulldown) {
+            int quickPulldownValue = Integer.valueOf((String) newValue);
+            Settings.System.putIntForUser(resolver, Settings.System.QS_QUICK_PULLDOWN,
+                    quickPulldownValue, UserHandle.USER_CURRENT);
+            updatePulldownSummary(quickPulldownValue);
+            return true;
+        }
+        return false;
+    }
 
-		        if (value == 0) {
-		            // Smart pulldown deactivated
-		            mSmartPulldown.setSummary(res.getString(R.string.smart_pulldown_off));
-		        } else {
-		            String type = null;
-		            switch (value) {
-		                case 1:
-		                    type = res.getString(R.string.smart_pulldown_dismissable);
-		                    break;
-		                case 2:
-		                    type = res.getString(R.string.smart_pulldown_persistent);
-		                    break;
-		                default:
-		                    type = res.getString(R.string.smart_pulldown_all);
-		                    break;
-		            }
-		            // Remove title capitalized formatting
-		            type = type.toLowerCase();
-		            mSmartPulldown.setSummary(res.getString(R.string.smart_pulldown_summary, type));
-		        }
-			}
+    private void updatePulldownSummary(int value) {
+        Resources res = getResources();
 
-		    private void updateQuickPulldownSummary(int value) {
-		        Resources res = getResources();
-
-		        if (value == 0) {
-		            // quick pulldown deactivated
-		            mQuickPulldown.setSummary(res.getString(R.string.quick_pulldown_off));
-		        } else {
-		            Locale l = Locale.getDefault();
-		            boolean isRtl = TextUtils.getLayoutDirectionFromLocale(l) == View.LAYOUT_DIRECTION_RTL;
-		            String direction = res.getString(value == 2
-		                    ? (isRtl ? R.string.quick_pulldown_right : R.string.quick_pulldown_left)
-		                    : (isRtl ? R.string.quick_pulldown_left : R.string.quick_pulldown_right));
-		            mQuickPulldown.setSummary(res.getString(R.string.summary_quick_pulldown, direction));
-		        }
-		    }
-			
+        if (value == 0) {
+            // quick pulldown deactivated
+            mQuickPulldown.setSummary(res.getString(R.string.quick_pulldown_off));
+        } else {
+            String direction = res.getString(value == 2
+                    ? R.string.quick_pulldown_summary_left
+                    : R.string.quick_pulldown_summary_right);
+            mQuickPulldown.setSummary(res.getString(R.string.quick_pulldown_summary, direction));
+        }
+    }
+	
 }
