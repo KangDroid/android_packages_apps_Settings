9e047056e0f6872d80ab48d872dea898f4c64d35
KangDroid <hyunwoo286@gmail.com>
2015-07-17 16:50:46 +0100

    Settings: Hide carrier on lock screen (2/2)
    From this commit: https://github.com/TeamBliss-LP/android_packages_apps_Settings/commit/d302812382aa123dfea8b3e410fd9480f11d2807
    
    (That commit was realy messy, so I did it my self)
    
    Change-Id: Ibe3f81b8fbbe1e1d387fe4b36fa8de5dd2dc7175

--- src/com/android/settings/kangdroid/KangDroidLockScreenSettings.java
+++ src/com/android/settings/kangdroid/KangDroidLockScreenSettings.java
@@ -14,30 +14,72 @@
 
 package com.android.settings.kangdroid;
 
+import android.app.admin.DevicePolicyManager;
+import android.app.ActivityManager;
 import android.os.Bundle;
-import android.preference.CheckBoxPreference;
+import android.os.UserHandle;
+import android.content.Context;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.provider.SearchIndexableResource;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+import android.os.Bundle;
+import android.os.UserHandle;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceCategory;
+import android.preference.PreferenceGroup;
 import android.preference.PreferenceScreen;
+import android.preference.SwitchPreference;
 import android.provider.Settings;
 import android.provider.Settings.SettingNotFoundException;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
+import com.android.settings.util.Helpers;
 
-public class KangDroidLockScreenSettings extends SettingsPreferenceFragment {
+public class KangDroidLockScreenSettings extends SettingsPreferenceFragment implements Preference.OnPreferenceChangeListener {
+	
+	private static final String CARRIERLABEL_ON_LOCKSCREEN="lock_screen_hide_carrier";
+	private SwitchPreference mCarrierLabelOnLockScreen;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.kangdroid_lockscreen_settings);
+		PreferenceScreen prefSet = getPreferenceScreen();
+		ContentResolver resolver = getActivity().getContentResolver();
+		
+        //CarrierLabel on LockScreen
+        mCarrierLabelOnLockScreen = (SwitchPreference) findPreference(CARRIERLABEL_ON_LOCKSCREEN);
+        if (!Utils.isWifiOnly(getActivity())) {
+            mCarrierLabelOnLockScreen.setOnPreferenceChangeListener(this);
+
+            boolean hideCarrierLabelOnLS = Settings.System.getInt(
+                    getActivity().getContentResolver(),
+                    Settings.System.LOCK_SCREEN_HIDE_CARRIER, 0) == 1;
+            mCarrierLabelOnLockScreen.setChecked(hideCarrierLabelOnLS);
+        } else {
+            prefSet.removePreference(mCarrierLabelOnLockScreen);
+        }
     }
 
     public boolean onPreferenceChange(Preference preference, Object objValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        final String key = preference.getKey();
+        if (preference == mCarrierLabelOnLockScreen) {
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.LOCK_SCREEN_HIDE_CARRIER,
+                    (Boolean) objValue ? 1 : 0);
+            Helpers.restartSystemUI();
+            return true;
+        }
         return false;
     }
 
