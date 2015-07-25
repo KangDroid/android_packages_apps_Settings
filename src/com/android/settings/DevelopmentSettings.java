a754a37de51318018e0169f0254b4481170b77d1
KangDroid <hyunwoo286@gmail.com>
2015-07-25 23:10:30 +0100

    Revert Global Action commits
    CM has more customizations
    
    Change-Id: I48bd7710955c52609589f9b141d4fd918eb434b7

--- src/com/android/settings/DevelopmentSettings.java
+++ src/com/android/settings/DevelopmentSettings.java
@@ -164,8 +164,6 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
 
     private static final String TERMINAL_APP_PACKAGE = "com.android.terminal";
 
-    private static final String ADVANCED_REBOOT_KEY = "advanced_reboot";
-
     private static final String HEADS_UP_TICKER_EXP_KEY = "heads_up_ticker_exp";
     private static final String DEVELOPMENT_SHORTCUT_KEY = "development_shortcut";
 
@@ -242,8 +240,6 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
     private SwitchPreference mShowAllANRs;
 
     private PreferenceScreen mProcessStats;
-
-    private SwitchPreference mAdvancedReboot;
     private SwitchPreference mHeadsUpTicker;
 
     private ListPreference mRootAccess;
@@ -317,7 +313,6 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
         mDebugViewAttributes = findAndInitSwitchPref(DEBUG_VIEW_ATTRIBUTES);
         mPassword = (PreferenceScreen) findPreference(LOCAL_BACKUP_PASSWORD);
         mAllPrefs.add(mPassword);
-        mAdvancedReboot = findAndInitSwitchPref(ADVANCED_REBOOT_KEY);
         mHeadsUpTicker = findAndInitSwitchPref(HEADS_UP_TICKER_EXP_KEY);
         mDevelopmentShortcut = findAndInitSwitchPref(DEVELOPMENT_SHORTCUT_KEY);
 
@@ -327,7 +322,6 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
             disableForUser(mClearAdbKeys);
             disableForUser(mEnableTerminal);
             disableForUser(mPassword);
-            disableForUser(mAdvancedReboot);
             disableForUser(mHeadsUpTicker);
             disableForUser(mDevelopmentShortcut);
         }
@@ -589,26 +583,9 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
         updateSimulateColorSpace();
         updateUseNuplayerOptions();
         updateUSBAudioOptions();
-        updateAdvancedRebootOptions();
         updateHeadsUpTickerOptions();
 		updateRootAccessOptions();
 		updateDevelopmentShortcutOptions();
-    }
-
-    private void resetAdvancedRebootOptions() {
-        Settings.Secure.putInt(getActivity().getContentResolver(),
-                Settings.Secure.ADVANCED_REBOOT, 0);
-    }
-
-    private void writeAdvancedRebootOptions() {
-        Settings.Secure.putInt(getActivity().getContentResolver(),
-                Settings.Secure.ADVANCED_REBOOT,
-                mAdvancedReboot.isChecked() ? 1 : 0);
-    }
-
-    private void updateAdvancedRebootOptions() {
-        mAdvancedReboot.setChecked(Settings.Secure.getInt(getActivity().getContentResolver(),
-                Settings.Secure.ADVANCED_REBOOT, 0) != 0);
     }
 
     private void resetHeadsUpTickerOptions() {
@@ -628,7 +605,7 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
     }
 
     private void updateDevelopmentShortcutOptions() {
-        mAdvancedReboot.setChecked(Settings.Secure.getInt(getActivity().getContentResolver(),
+        mDevelopmentShortcut.setChecked(Settings.Secure.getInt(getActivity().getContentResolver(),
                 Settings.Secure.DEVELOPMENT_SHORTCUT, 0) != 0);
     }
 
@@ -654,7 +631,6 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
         }
         resetDebuggerOptions();
         writeLogdSizeOption(null);
-        resetAdvancedRebootOptions();
         resetHeadsUpTickerOptions();
         resetRootAccessOptions();
         resetDevelopmentShortcutOptions();
@@ -1587,8 +1563,6 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
             writeUseAwesomePlayerOptions();
         } else if (preference == mUSBAudio) {
             writeUSBAudioOptions();
-        } else if (preference == mAdvancedReboot) {
-            writeAdvancedRebootOptions();
         } else if (preference == mHeadsUpTicker) {
             writeHeadsUpTickerOptions();
         } else if (preference == mDevelopmentShortcut) {
