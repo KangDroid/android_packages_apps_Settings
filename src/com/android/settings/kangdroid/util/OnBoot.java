ResurrectionRemix/Resurrection_packages_apps_Settings

Selinux Switch DU PS21
Change-Id: Idd38f844f94bdc81e2b690682c73e468156ea10b
PS3: update VariousSh*t and OnBoot logic with DU PS9
PS4: fix mess
PS5: Resolve conflicts after Floating Windows change
PS5: rebase

src/com/android/settings/rr/util/OnBoot.java
@@ -1,0 +1,33 @@
+package com.android.settings.rr.util;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.util.Log;
+
+import com.android.settings.util.CMDProcessor;
+
+import java.io.IOException;
+
+
+public class OnBoot extends BroadcastReceiver {
+
+    Context settingsContext = null;
+    private static final String TAG = "RR_onboot";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        try {
+            settingsContext = context.createPackageContext("com.android.settings", 0);
+        } catch (Exception e) {
+            Log.e(TAG, "Package not found", e);
+        }
+        SharedPreferences sharedpreferences = settingsContext.getSharedPreferences("com.android.settings_preferences", Context.MODE_PRIVATE);
+        if(sharedpreferences.getBoolean("selinux", true)) {
+            CMDProcessor.runSuCommand("setenforce 1");
+        } else if (!sharedpreferences.getBoolean("selinux", true)) {
+            CMDProcessor.runSuCommand("setenforce 0");
+        }
+    }
+}

KangDroid/android_packages_apps_Settings

src/com/android/settings/kangdroid/util/OnBoot.java
@@ -1,0 +1,33 @@
+package com.android.settings.kangdroid.util;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.util.Log;
+
+import com.android.settings.util.CMDProcessor;
+
+import java.io.IOException;
+
+
+public class OnBoot extends BroadcastReceiver {
+
+    Context settingsContext = null;
+    private static final String TAG = "KDP_onboot";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        try {
+            settingsContext = context.createPackageContext("com.android.settings", 0);
+        } catch (Exception e) {
+            Log.e(TAG, "Package not found", e);
+        }
+        SharedPreferences sharedpreferences = settingsContext.getSharedPreferences("com.android.settings_preferences", Context.MODE_PRIVATE);
+        if(sharedpreferences.getBoolean("selinux", true)) {
+            CMDProcessor.runSuCommand("setenforce 1");
+        } else if (!sharedpreferences.getBoolean("selinux", true)) {
+            CMDProcessor.runSuCommand("setenforce 0");
+        }
+    }
+}