e7d395562ec9675b11faffda6c417bad0a0859ef
Janson Kang <temasek71@gmail.com>
2015-09-04 18:58:50 +0800

    Settings: Make all 14 font variants available to Statusbar Clock (2/2)
    Change-Id: I866bf41b305537f556f2d11a5a94c7d0d33baefa
    Signed-off-by: Janson Kang <temasek71@gmail.com>

--- src/com/android/settings/kangdroid/KangDroidClockSettings.java
+++ src/com/android/settings/kangdroid/KangDroidClockSettings.java
@@ -299,7 +299,7 @@ public class KangDroidClockSettings extends SettingsPreferenceFragment
         mFontStyle.setOnPreferenceChangeListener(this);
         mFontStyle.setValue(Integer.toString(Settings.System.getInt(getActivity()
                 .getContentResolver(), Settings.System.STATUSBAR_CLOCK_FONT_STYLE,
-                5)));
+                0)));
         mFontStyle.setSummary(mFontStyle.getEntry());
 		
         mClockUseSecond = (SwitchPreference) prefSet.findPreference(CLOCK_USE_SECOND);
