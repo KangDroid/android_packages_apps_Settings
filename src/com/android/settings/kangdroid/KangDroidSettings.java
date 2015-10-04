7645862a707e4768fa7f12ade681c54c8280dca1
Janson Kang <temasek71@gmail.com>
2015-10-04 16:50:14 +0800

    Settings: Remove leftovers & fix build for heads up (2/2)
    Change-Id: I2e6c778055dc399a7b33ab8daf53feb9884ab445

--- src/com/android/settings/kangdroid/KangDroidSettings.java
+++ src/com/android/settings/kangdroid/KangDroidSettings.java
@@ -36,24 +36,16 @@ import java.util.List;
 
 public class KangDroidSettings extends SettingsPreferenceFragment implements Indexable, Preference.OnPreferenceChangeListener {
 	
-	private static final String KEY_HEADS_UP_SETTINGS = "heads_up_enabled";
-
-	private Preference mHeadsUp;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.kangdroid_settings);
-		mHeadsUp = findPreference(KEY_HEADS_UP_SETTINGS);
     }
 	
     @Override
     public void onResume() {
         super.onResume();
-        boolean headsUpEnabled = Settings.System.getInt(
-                getContentResolver(), Settings.System.HEADS_UP_USER_ENABLED, Settings.System.HEADS_UP_USER_ON) != 0;
-        mHeadsUp.setSummary(headsUpEnabled
-                ? R.string.summary_heads_up_enabled : R.string.summary_heads_up_disabled);
     }
 	
     public boolean onPreferenceChange(Preference preference, Object objValue) {
